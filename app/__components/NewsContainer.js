75c9afcba347a3bbbd9f61f6b383f5173fc34bb3
abhis12321 <abhisheksingh2359328@gmail.com>
2024-09-12 17:54:30 +0530

    pagination complited

--- app/__components/NewsContainer.js
+++ app/__components/NewsContainer.js
@@ -1,16 +1,32 @@
 "use client"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import NewsCard from './NewsCard'
 
-export default function NewsContainer({ articles , len}) {
+export default function NewsContainer({ articles , max}) {
     const [paginationatin , setPagination] = useState(0);
+    useEffect(() => {
+        console.log(paginationatin , max , articles.length);
+    }, [paginationatin]);
+
+    const handlePrevious = () => {
+        setPagination(prev => (prev - 1 + max) % max)
+    }
+    
+    const handleNext = () => {
+        setPagination(prev => (prev + 1) % max)
+    }
+    
   return (
     <>
         
       {
-        articles?.slice(paginationatin*12 , paginationatin*12+12)?.map((news , index) => <NewsCard key={index} article={news}>hello</NewsCard>)
+        articles?.slice(paginationatin*12 , paginationatin*12 + 12)?.map((news , index) => <NewsCard key={index} article={news} />)
       }
       
+      <div className="w-full flex items-center justify-around text-white font-bold text-sm">
+        <button className="w-[100px] py-2 text-center rounded-full bg-blue-600/80 hover:bg-blue-600" onClick={handlePrevious} >previous</button>
+        <button className="w-[100px] py-2 text-center rounded-full bg-blue-600/80 hover:bg-blue-600" onClick={handleNext}>next</button>
+      </div>
     </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,16 +1,14 @@
-import { articles } from './__components/articles';
-import NewsCard from './__components/NewsCard';
 import NewsContainer from './__components/NewsContainer';
-const url = `https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=${process.env.API_KEY}`;
-// const url = `https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=20&apikey=1855f8ce894cbf07951326030e9fb99b`;
+const url = `https://gnews.io/api/v4/search?q=example&lang=en&country=us&apikey=${process.env.API_KEY}`;
 
 export default async function page() {
-  // const data = await fetch(url)
-  //                         .then(res => res.json())
-  //                         .then(data => data.articles);  
+  const articles = await fetch(url)
+                          .then(res => res.json())
+                          .then(data => data.articles);  
+                          
   return (
-    <div className="flex flex-wrap p-4 gap-4 itemscenter justify-center bg-gray-100">
-      <NewsContainer articles={articles} len={articles.length}/>
+    <div className="min-h-screen flex flex-wrap p-4 gap-4 itemscenter justify-center bg-gray-100">
+      <NewsContainer articles={articles} max={Math.ceil(articles.length / 12)}/>
     </div>
   )
 }
