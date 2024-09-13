82e9026575fc5245551914544213ed881833c6f9
abhis12321 <abhisheksingh2359328@gmail.com>
2024-09-13 09:13:33 +0530

    Full info component is created for article to view the complete news

---
+++ app/__components/ActiveArticle.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function ActiveArticle({ article, cancelActive }) {
+    return (
+        <div className="flex flex-col gap-1 p-4 items-center justify-center">
+            <div onClick={cancelActive} className="w-fit text-3xl font-semibold hover:font-normal font-mono cursor-pointer py-2 px-[17px] rounded-full ring-2 ring-red-600 text-red-600 hover:text-white bg-red-600/30 hover:bg-red-600">X</div>
+            <div className="flex flex-col gap-4 p-4 bg-white shadow-[0_0_2px_gray] rounded-lg text-black">
+                <img src={article.image} className="rounded" />
+                <h2 className="text-2xl font-extrabold font-serif">{article.title}</h2>
+                <p className="text-sm font-mono font-semibold text-gray-700">{article.description}</p>
+                <p className="text-sm font-mono font-semibold text-gray-700">{article.content}</p>
+                <Link href={article.url} className="underline text-blue-800 hover:text-blue-600">visit the source</Link>
+            </div>
+        </div>
+    )
+}
--- app/__components/NewsContainer.js
+++ app/__components/NewsContainer.js
@@ -1,36 +1,40 @@
 "use client"
 import { useEffect, useState } from 'react'
 import NewsCard from './NewsCard'
+import ActiveArticle from './ActiveArticle';
 
-export default function NewsContainer({ articles , max}) {
-    const [paginationatin , setPagination] = useState(0);
-    const pages = Array.from({ length:max } , (_ , i) => i + 1);
-    
-    useEffect(() => {
-        console.log(paginationatin , max , articles.length);
-    }, [paginationatin]);
+export default function NewsContainer({ articles, max }) {
+  const [active, setActive] = useState();
+  const [paginationatin, setPagination] = useState(0);
+  const pages = Array.from({ length: max }, (_, i) => i + 1);
 
-    const handlePrevious = () => {
-        setPagination(prev => (prev - 1 + max) % max)
-    }
-    
-    const handleNext = () => {
-        setPagination(prev => (prev + 1) % max)
-    }
-    
+  useEffect(() => {
+    console.log(paginationatin, max, articles.length);
+  }, [paginationatin]);
+
+  const handlePrevious = () => {
+    setPagination(prev => (prev - 1 + max) % max)
+  }
+
+  const handleNext = () => {
+    setPagination(prev => (prev + 1) % max)
+  }
+
   return (
     <>
-        
+
       {
-        articles?.slice(paginationatin*12 , paginationatin*12 + 12)?.map((news , index) => <NewsCard key={index} article={news} />)
+        active ?  <ActiveArticle article={active} cancelActive={() => setActive(null)}/>
+        :
+        articles?.slice(paginationatin * 12, paginationatin * 12 + 12)?.map((news, index) => <NewsCard key={index} article={news} setActive={setActive} />)
       }
-      
+
       <div className="w-full flex flex-wrap items-center justify-around text-white font-bold text-sm">
         <button className={`py-[10px] px-5 text-4xl font-normal text-center rounded-full scale-y-125 shadow-[0_0_5px_white_inset] text-gray-300 ${paginationatin === 0 ? "bg-black/40 cursor-auto" : "bg-blue-600/80 hover:bg-blue-600 text-white"}`} onClick={handlePrevious} >&lt;</button>
         {
-          pages.map((pageNo , index) => <button className={`py-4 px-6 text-xl text-center rounded-lg ${paginationatin === index ? "ring-gray-950 ring-1 bg-green-700" : "bg-blue-600/80 hover:bg-blue-600"}`} onClick={() => setPagination(index)} key={index}>{ pageNo }</button>)
+          pages.map((pageNo, index) => <button className={`py-4 px-6 text-xl text-center rounded-lg ${paginationatin === index ? "ring-gray-950 ring-1 bg-green-700" : "bg-blue-600/80 hover:bg-blue-600"}`} onClick={() => setPagination(index)} key={index}>{pageNo}</button>)
         }
-        <button className={`py-[10px] px-5 text-4xl font-normal text-center rounded-full scale-y-125 shadow-[0_0_5px_white_inset] text-gray-300 ${paginationatin === max-1 ? "bg-black/40 cursor-auto" : "bg-blue-600/80 hover:bg-blue-600 text-white"}`} onClick={handleNext}>&gt;</button>
+        <button className={`py-[10px] px-5 text-4xl font-normal text-center rounded-full scale-y-125 shadow-[0_0_5px_white_inset] text-gray-300 ${paginationatin === max - 1 ? "bg-black/40 cursor-auto" : "bg-blue-600/80 hover:bg-blue-600 text-white"}`} onClick={handleNext}>&gt;</button>
       </div>
     </>
   )
