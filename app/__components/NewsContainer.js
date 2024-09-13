a6042e35a1f998a6d271790618306e35c64e5a2b
abhis12321 <abhisheksingh2359328@gmail.com>
2024-09-13 06:08:56 +0530

    pagination property is modified

--- app/__components/NewsContainer.js
+++ app/__components/NewsContainer.js
@@ -4,6 +4,8 @@ import NewsCard from './NewsCard'
 
 export default function NewsContainer({ articles , max}) {
     const [paginationatin , setPagination] = useState(0);
+    const pages = Array.from({ length:max } , (_ , i) => i + 1);
+    
     useEffect(() => {
         console.log(paginationatin , max , articles.length);
     }, [paginationatin]);
@@ -23,9 +25,12 @@ export default function NewsContainer({ articles , max}) {
         articles?.slice(paginationatin*12 , paginationatin*12 + 12)?.map((news , index) => <NewsCard key={index} article={news} />)
       }
       
-      <div className="w-full flex items-center justify-around text-white font-bold text-sm">
-        <button className="w-[100px] py-2 text-center rounded-full bg-blue-600/80 hover:bg-blue-600" onClick={handlePrevious} >previous</button>
-        <button className="w-[100px] py-2 text-center rounded-full bg-blue-600/80 hover:bg-blue-600" onClick={handleNext}>next</button>
+      <div className="w-full flex flex-wrap items-center justify-around text-white font-bold text-sm">
+        <button className={`py-[10px] px-5 text-4xl font-normal text-center rounded-full scale-y-125 shadow-[0_0_5px_white_inset] text-gray-300 ${paginationatin === 0 ? "bg-black/40 cursor-auto" : "bg-blue-600/80 hover:bg-blue-600 text-white"}`} onClick={handlePrevious} >&lt;</button>
+        {
+          pages.map((pageNo , index) => <button className={`py-4 px-6 text-xl text-center rounded-lg ${paginationatin === index ? "ring-gray-950 ring-1 bg-green-700" : "bg-blue-600/80 hover:bg-blue-600"}`} onClick={() => setPagination(index)} key={index}>{ pageNo }</button>)
+        }
+        <button className={`py-[10px] px-5 text-4xl font-normal text-center rounded-full scale-y-125 shadow-[0_0_5px_white_inset] text-gray-300 ${paginationatin === max-1 ? "bg-black/40 cursor-auto" : "bg-blue-600/80 hover:bg-blue-600 text-white"}`} onClick={handleNext}>&gt;</button>
       </div>
     </>
   )
